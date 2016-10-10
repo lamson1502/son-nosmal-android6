e5cdf6ad1d526df69ef8e634348b51c9b43fd207
NosMal <lamson19081997@gmail.com>
2016-10-11 01:26:37 +0700

    Added Session 3(MVC)

---
+++ Session1/src/Program.java
@@ -0,0 +1,8 @@
+/**
+ * Created by lamso on 10/3/2016.
+ */
+public class Program {
+    public static void main(String[] args){
+        GameWindow gameWindow = new GameWindow();
+    }
+}
---
+++ Session2/src/Program.java
@@ -0,0 +1,10 @@
+/**
+ * Created by lamso on 10/3/2016.
+ */
+public class Program {
+    public static void main(String[] args){
+        GameWindow gameWindow = new GameWindow();
+        Thread thread = new Thread(gameWindow);
+        thread.start();
+    }
+}
